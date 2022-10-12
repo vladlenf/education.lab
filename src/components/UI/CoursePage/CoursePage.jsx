fb1860d1f7d89372c83d5a61c3db0bf8e1b25077
Vladlen <frolov.vladlen02@gmail.com>
2022-10-12 23:16:46 +0300

    ver 0.02

--- src/components/UI/CoursePage/CoursePage.jsx
+++ src/components/UI/CoursePage/CoursePage.jsx
@@ -1,13 +1,43 @@
-import React from 'react';
-import classes from './CoursePage.module.css';
+import React, {useState} from 'react';
+import classes from './Course.module.css'
+import BurgerMenu from "../BurgerMenu/BurgerMenu";
+import MyButton from "../button/button";
+import AddNewTask from "../AddNewTask/AddNewTask";
+import Login from "../Login/Login";
+import MyModal from "../MyModal/MyModal";
+import AddKurs from "../AddKurs/AddKurs";
 
 const CoursePage = () => {
+    const [modal, setModal] = useState(false);
     return (
         <body>
             <div>
-                <header className="classes.headerCoursePage">
-                    <h1>Список доступных курсов</h1>
+                <header>
+                    <BurgerMenu/>
+                    <div className="classes.LeftHead">
+
+
+                    </div>
+
+                    <div className="classes.MiddleHead">
+                        <h1>Список доступных курсов</h1>
+                    </div>
+
+                    <div className="classes.RightHead">
+                        <MyButton onClick={() => setModal(true) }>
+                            Добавить список
+                        </MyButton>
+                    </div>
+                    <MyModal visible={modal} setVisible={setModal}>
+                        <AddKurs/>
+                    </MyModal>
+
                 </header>
+
+                <main className="classes.mainCourse">
+
+                    asdsassadasdasdsadsa
+                </main>
 
 
 
